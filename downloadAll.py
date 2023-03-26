23a2ebb3b490ee14c5c2ae083a6e8f534bc1c1af
AlzyWelzy <welzyalzy@gmail.com>
2023-03-26 14:57:41 +0530

    made new script

--- downloadAll.py
+++ downloadAll.py
@@ -16,13 +16,13 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
 # find all the repls on the page
-repls = soup.find_all("a", {"class": "sc-eXEjpC ReplitCardThumbnailCard"})
+repls = soup.find_all("div", {"class": "user-repl-item"})
 
 # loop through each repl and download it
 for repl in repls:
     # get the repl's name and URL
-    repl_name = repl["href"].split("/")[-1]
-    repl_url = f"https://replit.com{repl['href']}/archive.zip"
+    repl_name = repl.find("a")["href"].split("/")[-1]
+    repl_url = f"https://replit.com{repl.find('a')['href']}/archive.zip"
 
     # make a request to download the repl's source code
     response = requests.get(repl_url)
