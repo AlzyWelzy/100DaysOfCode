a7f5aa982867c5ad60b5b2a413aff1dcb40890a0
AlzyWelzy <welzyalzy@gmail.com>
2023-04-03 18:27:37 +0530

    added so many main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 
 if not os.path.exists("data"):
-    os.makedirs("data")
+    # os.makedirs("data")
+    os.mkdir("data")
 
 for i in range(0, 100):
     with open(f"data/file_{i+1}.txt", "w") as f:
         f.write("Hello World!")
+
+    # os.mkdir(f"data/day_{i+1}")
+    with open(f"data/day_{i+1}/main.py", "w") as f:
+        f.write("print('Hello World!')")
