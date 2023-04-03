354330e1492e3b4ff3fc30840f924b93fc38e351
AlzyWelzy <welzyalzy@gmail.com>
2023-04-03 18:24:48 +0530

    removing

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ if not os.path.exists("data"):
     os.makedirs("data")
 
 for i in range(0, 100):
-    with open("data/file_%s.txt" % i, "w") as f:
+    with open(f"data/file_{i+1}.txt", "w") as f:
         f.write("Hello World!")
