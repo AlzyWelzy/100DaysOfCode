55349ecd296db16e1228ee54071ed841f9c38a95
AlzyWelzy <welzyalzy@gmail.com>
2023-04-10 19:45:53 +0530

    made it in seconds

---
+++ tts/main.py
@@ -0,0 +1,11 @@
+import gtts
+from playsound import playsound
+
+# make request to google to get synthesis
+tts = gtts.gTTS("za warudo toki wo tomare")
+
+# save the audio file
+tts.save("za_warudo.mp3")
+
+# play the audio file
+playsound("za_warudo.mp3")
