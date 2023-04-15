6f0fc244a0405fc5056d5bf060317ff64193c6c1
AlzyWelzy <welzyalzy@gmail.com>
2023-04-15 10:26:31 +0530

    made it

--- imageresizer/other_implementation.py
+++ imageresizer/other_implementation.py
@@ -6,4 +6,24 @@ r = requests.get(url, allow_redirects=True)
 
 open("sunset.jpg", "wb").write(r.content)
 
-cv2.imshow("Original Image", cv2.imread("sunset.jpg"))
+src = "sunset.jpg"
+destination = "sunset_resized.jpg"
+
+cv2.imread(src, cv2.IMREAD_UNCHANGED)
+cv2.imshow("Original", src)
+
+# Percent by which the image is resized
+scale_percent = 60
+
+# Resize the image
+height = int(src.shape[0] * scale_percent / 100)
+width = int(src.shape[1] * scale_percent / 100)
+
+# dsize
+dsize = (width, height)
+
+# resize image
+output = cv2.resize(src, dsize)
+
+cv2.imwrite(destination, output)
+cv2.waitKey(0)
