e5aceb81bde5316a06007188a2567aea87af7ad7
AlzyWelzy <welzyalzy@gmail.com>
2023-04-15 10:27:45 +0530

    made it

--- imageresizer/other_implementation.py
+++ imageresizer/other_implementation.py
@@ -1,19 +1,22 @@
 import cv2
 import requests
 
+src = "sunset.jpg"
+destination = "sunset_resized.jpg"
+
+# Percent by which the image is resized
+scale_percent = 60
+
+
 url = "https://ik.imagekit.io/ikmedia/python-resizing/sunset_SLoRHsWVo.jpg"
 r = requests.get(url, allow_redirects=True)
 
-open("sunset.jpg", "wb").write(r.content)
+open(src, "wb").write(r.content)
 
-src = "sunset.jpg"
-destination = "sunset_resized.jpg"
 
 cv2.imread(src, cv2.IMREAD_UNCHANGED)
 cv2.imshow("Original", src)
 
-# Percent by which the image is resized
-scale_percent = 60
 
 # Resize the image
 height = int(src.shape[0] * scale_percent / 100)
